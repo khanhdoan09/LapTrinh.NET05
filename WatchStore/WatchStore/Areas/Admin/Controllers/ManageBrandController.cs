2b7e246dd8f4c9aeea156265e94a3535ca861985
minhchanh19130021 <93698433+minhchanh19130021@users.noreply.github.com>
2022-07-02 12:03:08 +0700

    thử nghiệm tải dữ liệu

--- WatchStore/WatchStore/Areas/Admin/Controllers/ManageBrandController.cs
+++ WatchStore/WatchStore/Areas/Admin/Controllers/ManageBrandController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using WatchStore.Models;
 
 namespace WatchStore.Areas.Admin.Controllers
 {
@@ -10,8 +12,9 @@ namespace WatchStore.Areas.Admin.Controllers
     {
         // GET: Admin/ManageBrand
         public ActionResult Index()
+
         {
-            return View();
+          return View();
         }
     }
 }
